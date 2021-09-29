2fd79aecfd54fa1326fdc24023a3b5b35c6d3f43
Rafael Henrique <fel.rique70@gmail.com>
2021-09-29 09:30:34 -0300

    3.3.2

--- src/main/java/com/mycompany/newmark/system/Sistema.java
+++ src/main/java/com/mycompany/newmark/system/Sistema.java
@@ -4,6 +4,25 @@ public class Sistema {
 
 	public static final String VERSAO = "3.3.1";
 	
-	// 1- Correção da duplicação do campo pesquisa por Pedido e por Id.
+	/* 3.3.2
+	 * 
+	 * Correção da pesquisa do pedido que exibia a informação do campo "Etiqueta" na
+	 * coluna "Subnúcleo". |Administracao - BuscaID |Chaves_Banco String SUBNUCLEO
+	 * |identificadorMateriaDAO - buscaIdentificador - buscaIdentificadorPorId
+	 * 
+	 * 
+	 * Adicionado campo etiqueta ao editar matéria através da janela
+	 * "Edição de Matéria Cadastrada". |Controller_TagEdicaoMateria JFXTextField
+	 * Etiqueta - Alterar. |IdentificadorMateriaDAO -atualizarIdentificadorMateria
+	 * 
+	 * Adicionado conversão da String para Maiúsculo dos campos pedido, complemento,
+	 * etiqueta ao inserir registro na janela "Identificador de Matérias" na
+	 * "Administração". |Administracao -inserirIdentificadorMateria
+	 * 
+	 * Correção da concatenação do Subnúcleo com a etiqueta. |Triagem_Etiquetas -
+	 * triarBanco |Processo_PetiçãoInicial - verificarNucleo
+	 * 
+	 */
+
 
 }
