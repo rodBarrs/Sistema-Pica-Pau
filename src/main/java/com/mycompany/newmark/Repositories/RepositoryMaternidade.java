522642b9f7840112bc718f92ad8427fcd2c294d1
rodBarrs <rodrigosbarros51@gmail.com>
2022-08-02 11:34:28 -0300

    aa

--- src/main/java/com/mycompany/newmark/Repositories/RepositoryMaternidade.java
+++ src/main/java/com/mycompany/newmark/Repositories/RepositoryMaternidade.java
@@ -49,8 +49,9 @@ public class RepositoryMaternidade {
         return "passou";
     }
 
-    public InformacoesDosprev coletarInformacoesDosprev (WebDriver driver, WebDriverWait wait){
-
+    public InformacoesDosprev coletarInformacoesDosprev (WebDriver driver, WebDriverWait wait) throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS).pageLoadTimeout(30, TimeUnit.SECONDS);
+        Thread.sleep(1000);
         InformacoesDosprev informacao = new InformacoesDosprev();
         String dataInicioMaisRecente = "zero";
         String nbMaisRecente ="";
@@ -83,11 +84,13 @@ public class RepositoryMaternidade {
             for(int i = 2; i < 30; i++) {
                 try {
                     String status = driver.findElement(By.xpath("/html/body/div/div[3]/table/tbody/tr[" + i + "]/td[6]")).getText();
+                    driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS).pageLoadTimeout(1, TimeUnit.SECONDS);
                     if (status.contains("INDEFERIDO")) {
                         String nbIndeferido = driver.findElement(By.xpath("/html/body/div/div[3]/table/tbody/tr[" + i + "]/td[1]")).getText();
                         String dataDeInicio = driver.findElement(By.xpath("/html/body/div/div[3]/table/tbody/tr[" + i + "]/td[3]")).getText();
                         if (dataInicioMaisRecente.contains("zero")) {
                             dataInicioMaisRecente = dataDeInicio;
+                            nbMaisRecente = nbIndeferido;
                         }
                         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
                         formatter = formatter.withLocale(Locale.US);
@@ -105,6 +108,8 @@ public class RepositoryMaternidade {
                     i = 30;
                 }
             }
+        WebElement TabelaTref = driver.findElement(By.className("conteudo"));
+        List<WebElement> listaEmpregos = new ArrayList<WebElement>(TabelaTref.findElements(By.cssSelector("tr")));
 
         informacao.setDataDeAjuizamento(dataAjuizamento);
         informacao.setSexo(sexo);
